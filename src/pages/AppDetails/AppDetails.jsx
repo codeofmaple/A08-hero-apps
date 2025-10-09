97664f851c9fdebaf634a443ebf4565e49754ff2
codeofmaple <maplecodetree@gmail.com>
2025-10-10 00:49:49 +0600

    made app-details responsive

--- src/pages/AppDetails/AppDetails.jsx
+++ src/pages/AppDetails/AppDetails.jsx
@@ -50,41 +50,44 @@ const AppDetails = () => {
 
     return (
         <div className='p-[4.16%] bg-[#62738210]'>
-            <div className='flex gap-10'>
-                <img src={image} alt="" className='size-[21.875rem]' />
+            <div className='flex md:flex-row flex-col gap-10'>
+
+                <img src={image} alt="" className='size-[21.875rem] self-center md:self-start' />
+
                 <div className='w-full'>
-                    <div>
+                    <div className='flex flex-col md:items-start items-center '>
                         <p className='font-bold text-[2rem] '>{title}</p>
                         <p className='details'>Developed by <span className='font-bold text-[#632ee3]'>{companyName}</span></p>
                     </div>
                     <hr className=' my-8 opacity-20' />
-                    <div className='flex gap-16'>
-                        <div>
-                            <img src={downloadIcon} alt="" />
+                    <div className='flex md:flex-row flex-col md:items-start items-center  md:justify-normal lg:gap-16 gap-5'>
+                        <div className='md:text-start text-center flex flex-col md:items-start  items-center'>
+                            <img src={downloadIcon} alt="" className='flex' />
                             <p className=' opacity-80 mt-2'>Downloads</p>
-                            <h2 className='font-extrabold text-[2.5rem]'>{downloads}M</h2>
+                            <h2 className='font-extrabold lg:text-[2.5rem] md:text-2xl text-[2.5rem]'>{downloads}M</h2>
                         </div>
-                        <div>
+                        <div className='md:text-start text-center flex flex-col md:items-start  items-center'>
                             <img src={ratingsIcon} alt="" />
-                            <p className=' opacity-80 mt-2'>Average Ratings</p>
-                            <h2 className='font-extrabold text-[2.5rem]'>{ratingAvg} </h2>
+                            <p className=' opacity-80 mt-2 text-nowrap'>Average Ratings</p>
+                            <h2 className='font-extrabold lg:text-[2.5rem] md:text-2xl text-[2.5rem]'>{ratingAvg} </h2>
                         </div>
-                        <div>
+                        <div className='md:text-start text-center flex flex-col md:items-start  items-center'>
                             <img src={reviewIcon} alt="" />
-                            <p className=' opacity-80 mt-2'>Total Reviews</p>
-                            <h2 className='font-extrabold text-[2.5rem]'> {reviews}K</h2>
+                            <p className=' opacity-80 mt-2 text-nowrap'>Total Reviews</p>
+                            <h2 className='font-extrabold lg:text-[2.5rem] md:text-2xl text-[2.5rem] '> {reviews}K</h2>
                         </div>
                     </div>
 
                     <button
                         onClick={handleInstall}
                         disabled={isInstalled}
-                        className={`mt-5 ${isInstalled ? 'opacity-70 cursor-not-allowed' : ''}`}
+                        className={`mt-5 w-full md:w-auto ${isInstalled ? 'opacity-70 cursor-not-allowed' : ''}`}
                     >
-                        <a className={`btn flex justify-center items-center ${isInstalled ? 'bg-gray-400' : 'bg-[#00d390]'} text-white px-10 py-5`}>
+                        <a className={`btn flex justify-center items-center ${isInstalled ? 'bg-gray-400' : 'bg-[#00d390]'} text-white px-10 py-5 w-full md:w-auto`}>
                             {isInstalled ? 'Installed' : `Install Now (${size}MB)`}
                         </a>
                     </button>
+
                 </div>
             </div>
             <hr className=' my-8 opacity-20' />
@@ -96,7 +99,7 @@ const AppDetails = () => {
 
             <hr className=' mb-8 mt-12 opacity-20' />
             <div className='space-y-4'>
-                <h5 className='font-semibold text-2xl'>Description</h5>
+                <h5 className='font-semibold text-2xl md:text-start text-center'>Description</h5>
                 <p className=' details'>{description}</p>
             </div>
 
